ea2f7a38aded71899cd5c4ea6c6e924da8526146
Boriusz <r.paliwoda1928@gmail.com>
2021-04-02 16:42:12 +0200

    Game ending, placements. Proper turn passing.

--- server_nest/src/game/game.interface.ts
+++ server_nest/src/game/game.interface.ts
@@ -1,9 +1,8 @@
 type Placement = 1 | 2 | 3 | 4;
-interface Finish {
-  player: PlayerData;
+export interface Finish {
+  player: PlayerData | PlayerDataRO;
   placement: Placement;
 }
-type FinishRO = PlayerDataRO[] & Placement;
 export interface GameData {
   players: PlayerData[];
   finished: Finish[];
@@ -27,7 +26,7 @@ export interface PlayerDataRO {
 
 export interface GameDataRO {
   players: PlayerDataRO[];
-  finished: FinishRO[];
+  finished: Finish[];
   currentTurn: Color;
   turnStatus: 1 | 2 | null;
   rolledNumber: 1 | 2 | 3 | 4 | 5 | 6 | null;
--- server_nest/src/game/game.service.ts
+++ server_nest/src/game/game.service.ts
@@ -3,6 +3,7 @@ import { RedisCacheService } from '../redisCache.service';
 import {
   Color,
   colorsValues,
+  Finish,
   GameData,
   GameDataRO,
   PlayerData,
@@ -44,6 +45,10 @@ export class GameService {
     const gameData = await this.redisCacheService.get(gameId);
     const { currentTurn, players } = gameData;
     const colorsInGame: Color[] = players
+      .filter(
+        (player: PlayerData) =>
+          !player.positions.every((position: number) => position === 105)
+      )
       .map((player: PlayerData) => player.color)
       .sort((a: Color, b: Color) => colorsValues[a] - colorsValues[b]);
     const currentTurnIndex = colorsInGame.findIndex(
@@ -52,6 +57,7 @@ export class GameService {
     if (currentTurnIndex === colorsInGame.length - 1)
       gameData.currentTurn = colorsInGame[0];
     else gameData.currentTurn = colorsInGame[currentTurnIndex + 1];
+
     await this.redisCacheService.set(gameId, gameData);
   }
 
@@ -68,7 +74,7 @@ export class GameService {
         (positions.every((position: number) => position === 0) &&
           newlyRolledNum !== 1 &&
           newlyRolledNum !== 6) || // Cant move anything bc everything is at start
-        positions.every((position: number) => position + rolledNumber > 105)
+        positions.every((position: number) => position + newlyRolledNum > 105)
       ) {
         gameData.rolledNumber = newlyRolledNum as 1 | 2 | 3 | 4 | 5 | 6;
         gameData.turnStatus = 2;
@@ -105,6 +111,7 @@ export class GameService {
     userId: string
   ): Promise<void> {
     const gameData = await this.redisCacheService.get(gameId);
+
     const isPlayersTurn = await this.checkIfIsPlayersTurn(gameData, userId);
     if (!isPlayersTurn) return;
     const { rolledNumber } = gameData;
@@ -122,6 +129,7 @@ export class GameService {
       if (player.positions.every((position: number) => position === 105)) {
         const data = await this.playerFinished(player, gameData);
         await this.redisCacheService.set(gameId, data);
+        await this.passTurnToNextPlayer(gameId);
         return;
       }
 
@@ -135,7 +143,11 @@ export class GameService {
   ): Promise<GameData> {
     const placement = (gameData.finished.length + 1) as 1 | 2 | 3 | 4;
     gameData.finished.push({ player, placement });
-    console.log(`Player ${player.name} finished game placing: ...`);
+    gameData.rolledNumber = null;
+    console.log(`Player ${player.name} finished game placing: ${placement}`);
+    if (gameData.finished.length === gameData.players.length)
+      console.log('game neded');
+
     return gameData;
   }
 
@@ -144,7 +156,6 @@ export class GameService {
     gameData: GameData,
     pawnId: number
   ): Promise<void> {
-    console.log('killer');
     const { players, currentTurn } = gameData;
     const otherPlayers = players.filter(
       (player: PlayerData) => player.color !== currentTurn
@@ -158,14 +169,12 @@ export class GameService {
         ? colorsValues[currentTurn] * 13 + player.positions[pawnId] - 52
         : colorsValues[currentTurn] * 13 + player.positions[pawnId];
     if (lastlyMovedPawn % 13 !== 1) {
-      console.log(`predator at pos ${lastlyMovedPawn}`);
       otherPlayers.forEach((player: PlayerData) => {
         player.positions = player.positions.map((position: number) => {
           const preyPosition =
             colorsValues[player.color] * 13 + position > 52
               ? colorsValues[player.color] * 13 + position - 52
               : colorsValues[player.color] * 13 + position;
-          console.log(`prey position: ${preyPosition}`);
           if (preyPosition === lastlyMovedPawn) return 0;
           return position;
         });
@@ -175,7 +184,6 @@ export class GameService {
       gameData.rolledNumber = null;
       gameData.turnStatus = 1;
       await this.redisCacheService.set(gameId, gameData);
-      console.log(await this.redisCacheService.get(gameId));
       return;
     }
     gameData.rolledNumber = null;
