871d48055d1a1f5b2233c1414771c012b3c3748f
Boriusz <r.paliwoda1928@gmail.com>
2021-04-10 11:10:15 +0200

    Deleted old server files

--- server/src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const TIME_BEFORE_START = 1000 * 10; // 10 secs
-export const PLAYER_COLORS = ["red", "blue", "green", "yellow"];
-export const colorsValues = {
-  red: 4,
-  blue: 3,
-  green: 2,
-  yellow: 1,
-};
